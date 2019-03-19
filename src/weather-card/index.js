8ab752ffb0e9b62b3f05a6b90c69eaa5c179281d
Darlan Mendonça <darlanmendonca@gmail.com>
2019-03-18 23:49:51 -0300

    begin styles for weather-card

--- src/weather-card/index.js
+++ src/weather-card/index.js
@@ -12,24 +12,64 @@ class WeatherCard extends Minimalist {
   }
 
   render(props = {}) {
+    const temperatureClassName = props.temperature <= 5
+      ? 'cold'
+      : props.temperature <= 25
+        ? 'normal'
+        : 'hot'
+
     return `
-      <div style="border: 1px solid black; display: inline-block">
-        <h2>${props.city}, ${props.state}</h2>
-        <h3>${props.temperature}º</h3>
-
-        ${
-          props.humidity
-            ? `<h4>Humidity ${props.humidity * 100}%</h4>`
-            : ''
+      <style>
+        .weather-card {
+          background: white;
+          font-family: Helvetica;
+          color: #737c84;
+          display: inline-block;
+          box-shadow: 0 0 10px rgba(55, 55, 55, .1);
+          text-align: center;
+          border-radius: 5px;
+          border: 1px solid #ebebeb;
         }
 
-        ${
-          props.pressure
-            ? `<h4>Pressure ${props.pressure}hPa</h4>`
-            : ''
+        .weather-card header {
+          line-height: 2em;
+          border-bottom: 1px solid #ebebeb;
         }
 
-        Updated at ${this.lastUpdate}
+        .weather-card strong {
+          font-size: 5em;
+          font-weight: normal;
+        }
+
+        .weather-card .cold {color: #69a3ff}
+        .weather-card .normal {color: #ff9632}
+        .weather-card .hot {color: #ed1946}
+
+        .weather-card footer {
+          box-shadow: 0 0 10px rgba(241, 241, 241, .5);
+          border-bottom-left-radius: inherit;
+          border-bottom-right-radius: inherit;
+        }
+      </style>
+      <div class="weather-card">
+        <header>${props.city}, ${props.state}</header>
+        <strong class="${temperatureClassName}">${props.temperature}º</strong>
+        
+        <footer>
+          ${
+            props.humidity
+              ? `<span>Humidity ${props.humidity * 100}%</span>`
+              : ''
+          }
+
+          ${
+            props.pressure
+              ? `<span>Pressure ${props.pressure}hPa</span>`
+              : ''
+          }
+
+          <div>Updated at ${this.lastUpdate}</div>
+        </footer>
       </div>
     `
   }
